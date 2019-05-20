4c6378574418ae752dbd801a5e3e0d4f2d5658bd
osmaryrp97 <50812890+osmaryrp97@users.noreply.github.com>
2019-05-19 23:49:58 -0500

    Initial commit

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Contador: " + count.ToString();
+        if (count == 30)
+        {
+            winText.text = "Ganaste!";
+            
+            SceneManager.LoadScene(0);
+        }
+    }
+}
